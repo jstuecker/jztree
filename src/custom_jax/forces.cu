da544e7749a93de03906d5ee98cb0518f697aad2
Jens Stücker <jens.stuecker@univie.ac.at>
2025-07-27 13:04:46 +0200

    Put actual potential, made mass flexible

--- src/custom_jax/forces.cu
+++ src/custom_jax/forces.cu
@@ -9,15 +9,15 @@ namespace ffi = xla::ffi;
 // And in Python:
 // (4) A Python function that calls the FFI handler
 
-__global__ void PotentialKernel(const float3 *x, float *phi, size_t n) {
+__global__ void PotentialKernel(const float4 *xm, float *phi, size_t n) {
   size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
   const size_t blocksize = blockDim.x;
 
   const size_t steps = n / blocksize;
 
-  float3 xi = x[blockIdx.x * blocksize + threadIdx.x];
+  float4 xmi = xm[blockIdx.x * blocksize + threadIdx.x];
 
-  extern __shared__ float3 xj[];
+  extern __shared__ float4 xmj_shared[];
 
   float phii = 0.f;
 
@@ -25,17 +25,21 @@ __global__ void PotentialKernel(const float3 *x, float *phi, size_t n) {
     // Load the next block of x into shared memory
     // this avoids reading from global memory multiple times
     __syncthreads();
-    xj[threadIdx.x] = x[jblock * blocksize + threadIdx.x];
+    xmj_shared[threadIdx.x] = xm[jblock * blocksize + threadIdx.x];
     __syncthreads();
 
     for (size_t j = 0; j < blocksize; j++) {
-      float3 xj_val = xj[j];
+      float4 xmj = xmj_shared[j];
       
-      float dx = xi.x - xj_val.x;
-      float dy = xi.y - xj_val.y;
-      float dz = xi.z - xj_val.z;
+      float dx = xmi.x - xmj.x;
+      float dy = xmi.y - xmj.y;
+      float dz = xmi.z - xmj.z;
+      float m = xmj.w; // we packed mass into the w component of xm
 
-      phii += sqrtf(dx*dx + dy*dy + dz*dz);
+      float r = sqrtf(dx*dx + dy*dy + dz*dz);
+      float rinv = (r >= 1e-15f) ? 1.0f/r : 0.0f; // avoid division by zero
+
+      phii += -m*rinv;
     }
   }
 
@@ -47,8 +51,8 @@ ffi::Error PotentialHost(cudaStream_t stream, ffi::Buffer<ffi::F32> x, ffi::Resu
 
   const size_t grid_size = (n + (block_size - 1)) / block_size;
   
-  auto* x_float3 = reinterpret_cast<const float3*>(x.typed_data()); // interprete x as an array of float3. This makes the kernel easier to write.
-  PotentialKernel<<<grid_size, block_size, /*shared_mem=*/block_size*sizeof(float3), stream>>>(x_float3, phi->typed_data(), n);
+  auto* x_float4 = reinterpret_cast<const float4*>(x.typed_data()); // interprete xm as an array of float4. This makes the kernel easier to write.
+  PotentialKernel<<<grid_size, block_size, /*shared_mem=*/block_size*sizeof(float4), stream>>>(x_float4, phi->typed_data(), n);
 
   cudaError_t last_error = cudaGetLastError();
   if (last_error != cudaSuccess) {
--- src/custom_jax/forces.py
+++ src/custom_jax/forces.py
@@ -21,12 +21,15 @@ library = ctypes.cdll.LoadLibrary(libfile)
 
 jax.ffi.register_ffi_target("potential", jax.ffi.pycapsule(library.Potential), platform="CUDA")
 
-def potential(x, block_size=64):
+def potential(x, mass=1., block_size=64):
   assert x.dtype == jnp.float32
   assert x.shape[-1] == 3
   assert x.ndim >= 2
   assert np.prod(x.shape[:-1]) % block_size == 0, "The length of x must be divisible by the block size."
   
+  # Reading on GPU will be more efficient if we read pos and mass together as single float4 values
+  xm = jnp.concatenate([x, jnp.broadcast_to(1., x.shape[:-1])[:,None]], axis=-1)
+  
   out_type = jax.ShapeDtypeStruct(x.shape[:-1], x.dtype)
-  phi = jax.ffi.ffi_call("potential", (out_type,))(x, n=np.uint64(block_size))
+  phi = jax.ffi.ffi_call("potential", (out_type,))(xm, n=np.uint64(block_size))
   return phi[0]
