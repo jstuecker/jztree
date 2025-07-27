86c9b5ae7cd320b890b15bab7c1a8709d0dd303a
Jens Stücker <jens.stuecker@univie.ac.at>
2025-07-27 16:52:32 +0200

    Use consistent formatting style accross different files

--- src/custom_jax/forces.cu
+++ src/custom_jax/forces.cu
@@ -9,9 +9,9 @@ namespace ffi = xla::ffi;
 // A wrapper to encapsulate an FFI call
 template <typename T>
 nb::capsule EncapsulateFfiCall(T *fn) {
-  static_assert(std::is_invocable_r_v<XLA_FFI_Error *, T, XLA_FFI_CallFrame *>,
-                "Encapsulated function must be and XLA FFI handler");
-  return nb::capsule(reinterpret_cast<void *>(fn));
+    static_assert(std::is_invocable_r_v<XLA_FFI_Error *, T, XLA_FFI_CallFrame *>,
+                  "Encapsulated function must be and XLA FFI handler");
+    return nb::capsule(reinterpret_cast<void *>(fn));
 }
 
 // Each custom FFI handler has four parts:
@@ -24,58 +24,57 @@ nb::capsule EncapsulateFfiCall(T *fn) {
 // (4) A Python function that calls the FFI handler
 
 __global__ void PotentialKernel(const float4 *xm, float *phi, size_t n, float epsilon) {
-  const size_t blocksize = blockDim.x;
-  const size_t steps = n / blocksize;
-  float epsilon2 = epsilon * epsilon;
+    const size_t blocksize = blockDim.x;
+    const size_t steps = n / blocksize;
+    float epsilon2 = epsilon * epsilon;
 
-  float4 xmi = xm[blockIdx.x * blocksize + threadIdx.x];
+    float4 xmi = xm[blockIdx.x * blocksize + threadIdx.x];
 
-  extern __shared__ float4 xmj_shared[];
+    extern __shared__ float4 xmj_shared[];
 
-  float phii = 0.f;
+    float phii = 0.f;
 
-  for (size_t jblock = 0; jblock < steps; jblock += 1) {
-    // Load the next block of x into shared memory
-    // this avoids reading from global memory multiple times
-    __syncthreads();
-    xmj_shared[threadIdx.x] = xm[jblock * blocksize + threadIdx.x];
-    __syncthreads();
+    for (size_t jblock = 0; jblock < steps; jblock += 1) {
+        // Load the next block of x into shared memory
+        // this avoids reading from global memory multiple times
+        __syncthreads();
+        xmj_shared[threadIdx.x] = xm[jblock * blocksize + threadIdx.x];
+        __syncthreads();
 
-    for (size_t j = 0; j < blocksize; j++) {
-      float4 xmj = xmj_shared[j];
-      
-      float dx = xmi.x - xmj.x;
-      float dy = xmi.y - xmj.y;
-      float dz = xmi.z - xmj.z;
-      float m = xmj.w; // we packed mass into the w component of xm
+        for (size_t j = 0; j < blocksize; j++) {
+            float4 xmj = xmj_shared[j];
+            
+            float dx = xmi.x - xmj.x;
+            float dy = xmi.y - xmj.y;
+            float dz = xmi.z - xmj.z;
+            float m = xmj.w; // we packed mass into the w component of xm
 
-      float r2 = dx*dx + dy*dy + dz*dz;
+            float r2 = dx*dx + dy*dy + dz*dz;
 
-      phii += -1.0f*m*rsqrtf(r2 + epsilon2);
+            phii += -1.0f*m*rsqrtf(r2 + epsilon2);
+        }
     }
-  }
 
-  // Since it is good to avoid any branches on GPU, we deal with the self-interaction by
-  // first adding it above and now subtracting it again:
-  phii += xmi.w / epsilon; 
+    // Since it is good to avoid any branches on GPU, we deal with the self-interaction by
+    // first adding it above and now subtracting it again:
+    phii += xmi.w / epsilon; 
 
-  phi[blockIdx.x * blocksize + threadIdx.x] = phii;
+    phi[blockIdx.x * blocksize + threadIdx.x] = phii;
 }
 
 ffi::Error PotentialHost(cudaStream_t stream, ffi::Buffer<ffi::F32> x, ffi::ResultBuffer<ffi::F32> phi, size_t block_size, float epsilon) {
-  size_t n = x.element_count() / x.dimensions().back();
+    size_t n = x.element_count() / x.dimensions().back();
 
-  const size_t grid_size = (n + (block_size - 1)) / block_size;
-  
-  auto* x_float4 = reinterpret_cast<const float4*>(x.typed_data()); // interprete xm as an array of float4. This makes the kernel easier to write.
-  PotentialKernel<<<grid_size, block_size, block_size*sizeof(float4), stream>>>(x_float4, phi->typed_data(), n, epsilon);
+    const size_t grid_size = (n + (block_size - 1)) / block_size;
 
-  cudaError_t last_error = cudaGetLastError();
-  if (last_error != cudaSuccess) {
-    return ffi::Error::Internal(
-        std::string("CUDA error: ") + cudaGetErrorString(last_error));
-  }
-  return ffi::Error::Success();
+    auto* x_float4 = reinterpret_cast<const float4*>(x.typed_data()); // interprete xm as an array of float4. This makes the kernel easier to write.
+    PotentialKernel<<<grid_size, block_size, block_size*sizeof(float4), stream>>>(x_float4, phi->typed_data(), n, epsilon);
+
+    cudaError_t last_error = cudaGetLastError();
+    if (last_error != cudaSuccess) {
+        return ffi::Error::Internal(std::string("CUDA error: ") + cudaGetErrorString(last_error));
+    }
+    return ffi::Error::Success();
 }
 
 XLA_FFI_DEFINE_HANDLER_SYMBOL(
@@ -89,5 +88,5 @@ XLA_FFI_DEFINE_HANDLER_SYMBOL(
     {xla::ffi::Traits::kCmdBufferCompatible});
 
 NB_MODULE(nb_forces, m) {
-  m.def("potential", []() { return EncapsulateFfiCall(Potential); });
+    m.def("potential", []() { return EncapsulateFfiCall(Potential); });
 }
--- src/custom_jax/forces.py
+++ src/custom_jax/forces.py
@@ -13,15 +13,15 @@ import custom_jax.nb_forces as nb_forces
 jax.ffi.register_ffi_target("potential", nb_forces.potential(), platform="CUDA")
 
 def potential(x, mass=1., block_size=64, eps=1e-2):
-  assert x.dtype == jnp.float32
-  assert x.shape[-1] == 3
-  assert x.ndim >= 2
-  assert np.prod(x.shape[:-1]) % block_size == 0, "The length of x must be divisible by the block size."
-  assert eps > 0, "Epsilon must be positive to deal with self-interaction."
+    assert x.dtype == jnp.float32
+    assert x.shape[-1] == 3
+    assert x.ndim >= 2
+    assert np.prod(x.shape[:-1]) % block_size == 0, "The length of x must be divisible by the block size."
+    assert eps > 0, "Epsilon must be positive to deal with self-interaction."
 
-  # Reading on GPU will be more efficient if we read pos and mass together as single float4 values
-  xm = jnp.concatenate([x, jnp.broadcast_to(1., x.shape[:-1])[:,None]], axis=-1)
-  
-  out_type = jax.ShapeDtypeStruct(x.shape[:-1], x.dtype)
-  phi = jax.ffi.ffi_call("potential", (out_type,))(xm, block_size=np.uint64(block_size), epsilon=np.float32(eps))
-  return phi[0]
+    # Reading on GPU will be more efficient if we read pos and mass together as single float4 values
+    xm = jnp.concatenate([x, jnp.broadcast_to(1., x.shape[:-1])[:,None]], axis=-1)
+
+    out_type = jax.ShapeDtypeStruct(x.shape[:-1], x.dtype)
+    phi = jax.ffi.ffi_call("potential", (out_type,))(xm, block_size=np.uint64(block_size), epsilon=np.float32(eps))
+    return phi[0]
