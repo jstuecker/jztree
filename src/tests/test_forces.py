51347237577b4a9f710037c824f7242dd0b0f1b3
Jens Stücker <jens.stuecker@univie.ac.at>
2025-07-28 23:22:16 +0200

    Added unit tests

---
+++ src/tests/test_forces.py
@@ -0,0 +1,48 @@
+import pytest
+import custom_jax as cj
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+def test_potential():
+    x = jax.random.normal(jax.random.PRNGKey(0), (8*1024, 3)).block_until_ready()
+
+    phi0 = cj.forces.potential_pure_jax_jit(x, eps=1e-2)
+
+    for block_size in (32, 64, 128):
+        phi1 = cj.forces.potential_jit(x, block_size=block_size, eps=1e-2)
+
+        assert np.allclose(phi0, phi1, rtol=1e-4)
+
+def test_force():
+    x = jax.random.normal(jax.random.PRNGKey(0), (8*1024, 3)).block_until_ready()
+
+    phi0 = cj.forces.potential_pure_jax_jit(x, eps=1e-2)
+    acc0 = cj.forces.force_pure_jax_jit(x, eps=1e-2)
+
+    for block_size in (32, 64, 128):
+        acc1 = cj.forces.force_jit(x, block_size=block_size, eps=1e-2)
+        acc2, phi2 =  cj.forces.force_jit(x, block_size=32, eps=1e-2, get_potential=True)
+
+        assert np.allclose(acc0, acc1, rtol=1e-2)
+        assert np.allclose(acc0, acc2, rtol=1e-2)
+        assert np.allclose(phi0, phi2, rtol=1e-2)
+
+def test_ilist_force():
+    for n in 1024, 3333, 13231:
+        x = jax.random.normal(jax.random.PRNGKey(0), (n, 3)).block_until_ready()
+
+        phi0 = cj.forces.potential_pure_jax_jit(x, eps=1e-2)
+        acc0 = cj.forces.force_pure_jax_jit(x, eps=1e-2)
+
+        isplit = jnp.append(jnp.arange(0, n, 32), jnp.array([n]))
+        # Add some random shifts so that we don't always have the same number of interactions
+        isplit = isplit.at[1:-1].add(jax.random.randint(jax.random.PRNGKey(0), len(isplit) - 2, -16, 16))
+
+        for block_size, interactions_per_block in ((32, None), (32, 7), (64, 16), (128, 99)): 
+            # Any combination of block size and interactions per block should be valid.
+            # These are just parameters that may affect execution speed
+            acc1, phi1 = cj.forces.ilist_force_jit(x, isplit, block_size=block_size, interactions_per_block=interactions_per_block, eps=1e-2, get_potential=True)
+
+            assert np.allclose(acc0, acc1, atol=1e-2)
+            assert np.allclose(phi0, phi1, rtol=1e-2)
