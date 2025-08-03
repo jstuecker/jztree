ca37bbbfad5f3292ac7235b6d1a8d448b423adb1
Jens Stücker <jens.stuecker@univie.ac.at>
2025-08-03 20:10:43 +0200

    Left parent search working

--- src/custom_jax/tree.cu
+++ src/custom_jax/tree.cu
@@ -110,7 +110,7 @@ ffi::Error HostPosZorderSort(cudaStream_t stream, ffi::Buffer<ffi::F32> pos_in, 
 }
 
 
-__global__ void GetMsbDiffLevel(const float3* pos_in, int *level_out, size_t n) {
+__global__ void KernelMsbDiffLevel(const float3* pos_in, int *level_out, size_t n) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n-1) {
         float3 p1 = pos_in[idx];
@@ -120,14 +120,58 @@ __global__ void GetMsbDiffLevel(const float3* pos_in, int *level_out, size_t n) 
         int msb_y = float_xor_msb(p1.y, p2.y);
         int msb_z = float_xor_msb(p1.z, p2.z);
 
-        // The most significant differing bit
-        // int msb_diff = max(3*msb_x+2, max(3*msb_y+1, 3*msb_z));
-
-        level_out[idx] = msb_x;
-        level_out[idx+(n-1)] = msb_y;
-        level_out[idx+2*(n-1)] = msb_z;
-        level_out[idx+3*(n-1)] = max(3*msb_x+3, max(3*msb_y+2, 3*msb_z+1));
+        // The level is given by the most significant differing bit
+        // but offset according to the dimension
+        // This is so that L = 2**(level//3) would correspond to the size 
+        // of the octree-node that contains both points
+        // The minimal possible difference is -(127+23)*3 = -450
+        // the maximal possible difference is (128+1)*3 = 387
+        level_out[idx] = max(3*msb_x+3, max(3*msb_y+2, 3*msb_z+1));
     }
+}
+
+__device__ __forceinline__ int32_t msb_diff_level(const float3 &p1, const float3 &p2) {
+    int msb_x = float_xor_msb(p1.x, p2.x);
+    int msb_y = float_xor_msb(p1.y, p2.y);
+    int msb_z = float_xor_msb(p1.z, p2.z);
+
+    // The level is given by the most significant differing bit
+    // but offset according to the dimension
+    return max(3*msb_x+3, max(3*msb_y+2, 3*msb_z+1));
+}
+
+__global__ void KernelBinarySearchLeftParent(const float3* pos_in, int *tree_info, size_t n) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= n-1)
+        return;
+    
+    // Calculate the level difference of our considered set of two points (=node)
+    float3 p1 = pos_in[idx];
+    float3 p2 = pos_in[idx + 1];
+
+    int target_level = msb_diff_level(p1, p2);
+
+    // We do a binary search, trying to find the closest point to the left
+    // that has a level difference of at least `level`
+    size_t imin = -1;
+    size_t imax = idx+1;
+    int test_level = -450;
+    while (imin+1 < imax) {
+        size_t itest = (imin + imax) / 2;
+        test_level = msb_diff_level(pos_in[itest], p2);
+        if (test_level > target_level) {
+            imin = itest;
+        } else {
+            imax = itest;
+        }
+    }
+
+    // Our array has two fake nodes at the beginning and end
+    // that's why we have to offset the indices by 1
+    imin = imin+1;
+
+    tree_info[idx+1] = target_level;
+    tree_info[idx+1 + (n+1)] = imin;
 }
 
 ffi::Error HostBuildZTree(cudaStream_t stream, ffi::Buffer<ffi::F32> pos_in, ffi::ResultBuffer<ffi::S32> level_out, size_t block_size) {
@@ -136,7 +180,9 @@ ffi::Error HostBuildZTree(cudaStream_t stream, ffi::Buffer<ffi::F32> pos_in, ffi
     float3* keys_in = reinterpret_cast<float3*>(pos_in.typed_data());
     
     int blocks = (n + block_size - 1) / block_size;
-    GetMsbDiffLevel<<<blocks, block_size, 0, stream>>>(keys_in, level_out->typed_data(), n);
+    // KernelMsbDiffLevel<<<blocks, block_size, 0, stream>>>(keys_in, level_out->typed_data(), n);
+
+    KernelBinarySearchLeftParent<<<blocks, block_size, 0, stream>>>(keys_in, level_out->typed_data(), n);
 
     cudaError_t last_error = cudaGetLastError();
     if (last_error != cudaSuccess) {
@@ -168,17 +214,17 @@ XLA_FFI_DEFINE_HANDLER_SYMBOL(
 // Include deprecated functions
 // This module includes a bunch of functions that we do not need anymore, but we keep
 // them temporarily for comparison and test purposes
-#include "tree_deprecated.cu"
+// #include "tree_deprecated.cu"
 
 
 NB_MODULE(nb_tree, m) {
     m.def("PosZorderSort", []() { return EncapsulateFfiCall(PosZorderSort); });
     m.def("BuildZTree", []() { return EncapsulateFfiCall(BuildZTree); });
 
-    // A bunc of deprecated functions
-    m.def("OldArgsort", []() { return EncapsulateFfiCall(OldArgsort); });
-    m.def("OldI3zsort", []() { return EncapsulateFfiCall(OldI3zsort); });
-    m.def("OldF3zsort", []() { return EncapsulateFfiCall(OldF3zsort); });
-    m.def("OldI3Argsort", []() { return EncapsulateFfiCall(OldI3Argsort); });
-    m.def("OldI3zMergesort", []() { return EncapsulateFfiCall(OldI3zMergesort); });
+    // A bunch of deprecated functions
+    // m.def("OldArgsort", []() { return EncapsulateFfiCall(OldArgsort); });
+    // m.def("OldI3zsort", []() { return EncapsulateFfiCall(OldI3zsort); });
+    // m.def("OldF3zsort", []() { return EncapsulateFfiCall(OldF3zsort); });
+    // m.def("OldI3Argsort", []() { return EncapsulateFfiCall(OldI3Argsort); });
+    // m.def("OldI3zMergesort", []() { return EncapsulateFfiCall(OldI3zMergesort); });
 }
--- src/custom_jax/tree.py
+++ src/custom_jax/tree.py
@@ -27,7 +27,7 @@ def build_ztree(x, block_size=64):
     assert x.dtype == jnp.float32
     assert x.shape[-1] == 3
 
-    out_type = jax.ShapeDtypeStruct((5,x.shape[0]-1), jnp.int32)
+    out_type = jax.ShapeDtypeStruct((5,x.shape[0]+1), jnp.int32)
     ztree = jax.ffi.ffi_call("BuildZTree", (out_type,))(x, block_size=np.uint64(block_size))[0]
 
     return ztree
@@ -37,53 +37,54 @@ build_ztree.jit = jax.jit(build_ztree, static_argnames=("block_size",))
 # ================================= Deprecated functions   ======================================= #
 # They will be deleted later, for now we keep them for comparison purposes
 
-jax.ffi.register_ffi_target("OldArgsort", nb_tree.OldArgsort(), platform="CUDA")
-jax.ffi.register_ffi_target("OldI3zsort", nb_tree.OldI3zsort(), platform="CUDA")
-jax.ffi.register_ffi_target("OldF3zsort", nb_tree.OldF3zsort(), platform="CUDA")
-jax.ffi.register_ffi_target("OldI3Argsort", nb_tree.OldI3Argsort(), platform="CUDA")
-jax.ffi.register_ffi_target("OldI3zMergesort", nb_tree.OldI3zMergesort(), platform="CUDA")
+if hasattr(nb_tree, "OldArgsort"):
+    jax.ffi.register_ffi_target("OldArgsort", nb_tree.OldArgsort(), platform="CUDA")
+    jax.ffi.register_ffi_target("OldI3zsort", nb_tree.OldI3zsort(), platform="CUDA")
+    jax.ffi.register_ffi_target("OldF3zsort", nb_tree.OldF3zsort(), platform="CUDA")
+    jax.ffi.register_ffi_target("OldI3Argsort", nb_tree.OldI3Argsort(), platform="CUDA")
+    jax.ffi.register_ffi_target("OldI3zMergesort", nb_tree.OldI3zMergesort(), platform="CUDA")
 
-def old_argsort_cubradix(key, block_size=64):
-    assert key.dtype == jnp.int32
+    def old_argsort_cubradix(key, block_size=64):
+        assert key.dtype == jnp.int32
 
-    out_type = jax.ShapeDtypeStruct(key.shape[0:1], jnp.int32)
-    isort = jax.ffi.ffi_call("OldArgsort", (out_type,))(key, block_size=np.uint64(block_size))
-    return isort[0]
-old_argsort_cubradix.jit = jax.jit(old_argsort_cubradix, static_argnames=("block_size",))
+        out_type = jax.ShapeDtypeStruct(key.shape[0:1], jnp.int32)
+        isort = jax.ffi.ffi_call("OldArgsort", (out_type,))(key, block_size=np.uint64(block_size))
+        return isort[0]
+    old_argsort_cubradix.jit = jax.jit(old_argsort_cubradix, static_argnames=("block_size",))
 
-def old_i3zsort_thrust(ids, block_size=64):
-    assert ids.dtype == jnp.int32
-    assert ids.shape[-1] == 3
+    def old_i3zsort_thrust(ids, block_size=64):
+        assert ids.dtype == jnp.int32
+        assert ids.shape[-1] == 3
 
-    out_type = jax.ShapeDtypeStruct(ids.shape[0:1], jnp.int32)
-    isort = jax.ffi.ffi_call("OldI3zsort", (out_type,))(ids, block_size=np.uint64(block_size))
-    return isort[0]
-old_i3zsort_thrust.jit = jax.jit(old_i3zsort_thrust, static_argnames=("block_size",))
+        out_type = jax.ShapeDtypeStruct(ids.shape[0:1], jnp.int32)
+        isort = jax.ffi.ffi_call("OldI3zsort", (out_type,))(ids, block_size=np.uint64(block_size))
+        return isort[0]
+    old_i3zsort_thrust.jit = jax.jit(old_i3zsort_thrust, static_argnames=("block_size",))
 
-def old_f3zsort_thrust(x, block_size=64):
-    assert x.dtype == jnp.float32
-    assert x.shape[-1] == 3
+    def old_f3zsort_thrust(x, block_size=64):
+        assert x.dtype == jnp.float32
+        assert x.shape[-1] == 3
 
-    out_type = jax.ShapeDtypeStruct(x.shape[0:1], jnp.int32)
+        out_type = jax.ShapeDtypeStruct(x.shape[0:1], jnp.int32)
 
-    isort = jax.ffi.ffi_call("OldF3zsort", (out_type,))(x, block_size=np.uint64(block_size))
-    return isort[0]
-old_f3zsort_thrust.jit = jax.jit(old_f3zsort_thrust, static_argnames=("block_size",))
+        isort = jax.ffi.ffi_call("OldF3zsort", (out_type,))(x, block_size=np.uint64(block_size))
+        return isort[0]
+    old_f3zsort_thrust.jit = jax.jit(old_f3zsort_thrust, static_argnames=("block_size",))
 
-def old_i3argsort_cubradix(ids, block_size=64):
-    assert ids.dtype == jnp.int32
-    assert ids.shape[-1] == 3
+    def old_i3argsort_cubradix(ids, block_size=64):
+        assert ids.dtype == jnp.int32
+        assert ids.shape[-1] == 3
 
-    out_type = jax.ShapeDtypeStruct(ids.shape[0:1], jnp.int32)
-    isort = jax.ffi.ffi_call("OldI3Argsort", (out_type,))(ids, block_size=np.uint64(block_size))
-    return isort[0]
-old_i3argsort_cubradix.jit = jax.jit(old_i3argsort_cubradix, static_argnames=("block_size",))
+        out_type = jax.ShapeDtypeStruct(ids.shape[0:1], jnp.int32)
+        isort = jax.ffi.ffi_call("OldI3Argsort", (out_type,))(ids, block_size=np.uint64(block_size))
+        return isort[0]
+    old_i3argsort_cubradix.jit = jax.jit(old_i3argsort_cubradix, static_argnames=("block_size",))
 
-def old_i3zsort_cubmerge(ids, block_size=64):
-    assert ids.dtype == jnp.int32
-    assert ids.shape[-1] == 3
+    def old_i3zsort_cubmerge(ids, block_size=64):
+        assert ids.dtype == jnp.int32
+        assert ids.shape[-1] == 3
 
-    out_type = jax.ShapeDtypeStruct((ids.shape[0],4), jnp.int32)
-    isort = jax.ffi.ffi_call("OldI3zMergesort", (out_type,))(ids, block_size=np.uint64(block_size))
-    return isort[0][:,3]
-old_i3zsort_cubmerge.jit = jax.jit(old_i3zsort_cubmerge, static_argnames=("block_size",))
+        out_type = jax.ShapeDtypeStruct((ids.shape[0],4), jnp.int32)
+        isort = jax.ffi.ffi_call("OldI3zMergesort", (out_type,))(ids, block_size=np.uint64(block_size))
+        return isort[0][:,3]
+    old_i3zsort_cubmerge.jit = jax.jit(old_i3zsort_cubmerge, static_argnames=("block_size",))
