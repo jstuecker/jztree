cdd47e9b5a9abb2092eb620576b4c47c45f506fb
Jens Stücker <jens.stuecker@univie.ac.at>
2025-08-17 21:59:06 +0200

    Connected all interaction functions

--- src/custom_jax/fmdj_plugin.py
+++ src/custom_jax/fmdj_plugin.py
@@ -1,33 +1,29 @@
-# custom_jax/fmdj_plugin.py
 from fmdj.variants import Variant, VariantManager, vm, has_gpu
 from fmdj import config
 import custom_jax as cj
 import jax.numpy as jnp
 
-def test_func(x, cfg):
-    return x
-
-tag = "cj"
-priority = 1
-def only_register_if(cfg):
-    return has_gpu()
-# def reg(op, fn):
-#     vm.reg
-    # register_variant(op, name=name, fn=fn, priority=priority, only_if=only_register_if)
+TAG = "cuda"
 
 variants = VariantManager()
 
+def _ilist_node_to_node_cj(xnodes, multipoles, interactions, irange, cfg : config.Config):
+    return cj.multipoles.ilist_node_to_node(xnodes, multipoles, interactions, irange=irange, p=cfg.p, eps=cfg.softening)
+
+def _ilist_leaf_to_node_cj(xnodes, xpart, mpart, leaf_bounds, interactions, irange, cfg : config.Config):
+    return cj.multipoles.ilist_leaf_to_node(xnodes, xpart, mpart, leaf_bounds, jnp.abs(interactions), irange, p=cfg.p, eps=cfg.softening)
+
 def _ilist_leaf_to_leaf_cj(xpart, mpart, leaf_bounds, interactions, irange, cfg : config.Config):
-    print("cj ilist_leaf_to_leaf called with eps=%f" % cfg.softening)
     xm = jnp.concatenate([xpart, mpart[:, None]], axis=-1)
     return cj.forces.ilist_fphi(xm, leaf_bounds, jnp.abs(interactions), irange, eps=cfg.softening)[...,3]
 
 def register():
     if has_gpu():
-        variants.testfunc[tag] = Variant(test_func)
-        variants.ilist_leaf_to_leaf[tag] = Variant(_ilist_leaf_to_leaf_cj)
+        variants.ilist_node_to_node[TAG] = Variant(_ilist_node_to_node_cj)
+        variants.ilist_leaf_to_node[TAG] = Variant(_ilist_leaf_to_node_cj)
+        variants.ilist_leaf_to_leaf[TAG] = Variant(_ilist_leaf_to_leaf_cj)
 
         vm.register_variants(variants)
         print("Custom Jax Plugin registered!")
     else:
-        print("Could not find GPU, Custom Jax Plugin not registered.")
+        print("Could not find GPU, Custom Jax Plugin not registered!")
--- src/custom_jax/multipoles.py
+++ src/custom_jax/multipoles.py
@@ -12,51 +12,51 @@ jax.ffi.register_ffi_target("ilist_leaf2node_m2l", nb_multipoles.ilist_leaf2node
 
 # ======= Multipole Translators =======
 
-def ilist_multipole_to_local(mp, x, interactions=None, iminmax=None, p=1, block_size=32, interactions_per_block=None, eps=1e-2):
-    assert x.dtype == jnp.float32
-    assert mp.dtype == jnp.float32
-    assert x.ndim >= 2 and mp.ndim >= 2 and mp.ndim >= 2
-    assert x.shape[-1] == 3
-    assert mp.shape[-1] == ((p+3)*(p+2)*(p+1)) // 6
+def ilist_node_to_node(xnodes, multipoles, interactions, irange=None, p=1, block_size=32, interactions_per_block=None, eps=1e-2):
+    assert xnodes.dtype == jnp.float32
+    assert multipoles.dtype == jnp.float32
+    assert xnodes.ndim >= 2 and multipoles.ndim >= 2 and multipoles.ndim >= 2
+    assert xnodes.shape[-1] == 3
+    assert multipoles.shape[-1] == ((p+3)*(p+2)*(p+1)) // 6
 
     if interactions is None:
-        iarange = jnp.arange(0, len(mp))
+        iarange = jnp.arange(0, len(multipoles))
         interactions = np.stack((iarange, iarange), axis=-1).astype(np.int32)
-    if iminmax is None:
-        iminmax = jnp.array([0, len(interactions)], dtype=jnp.int32)
+    if irange is None:
+        irange = jnp.array([0, len(interactions)], dtype=jnp.int32)
     if interactions_per_block is None:
         interactions_per_block = np.clip(len(interactions) // (8096*block_size), 1, 256)
 
-    out_type = jax.ShapeDtypeStruct(mp.shape, mp.dtype)
-    loc = jax.ffi.ffi_call("ilist_m2l", (out_type,))(x, mp, interactions, iminmax, p=np.int32(p), block_size=np.uint64(block_size), interactions_per_block=np.uint64(interactions_per_block), epsilon=np.float32(eps))[0]
+    out_type = jax.ShapeDtypeStruct(multipoles.shape, multipoles.dtype)
+    loc = jax.ffi.ffi_call("ilist_m2l", (out_type,))(xnodes, multipoles, interactions, irange, p=np.int32(p), block_size=np.uint64(block_size), interactions_per_block=np.uint64(interactions_per_block), epsilon=np.float32(eps))[0]
     
     return loc
-ilist_multipole_to_local.jit = jax.jit(ilist_multipole_to_local, static_argnames=("p", "block_size", "eps"))
+ilist_node_to_node.jit = jax.jit(ilist_node_to_node, static_argnames=("p", "block_size", "eps"))
 
-def ilist_leaf_to_local(xnodes, xpart, mpart, isplit,  interactions, iminmax=None, 
+def ilist_leaf_to_node(xnodes, xpart, mpart, isplit,  interactions, irange=None, 
                         p=1, interactions_per_block=None, eps=1e-2):
     assert xnodes.dtype == jnp.float32
     assert xpart.dtype == jnp.float32
     assert mpart.dtype == jnp.float32
     assert isplit.dtype == jnp.int32
     assert interactions.dtype == jnp.int32
-    assert iminmax is None or iminmax.dtype == jnp.int32
+    assert irange is None or irange.dtype == jnp.int32
 
     assert xnodes.ndim >= 2 and xpart.ndim >= 2
     xm = jnp.concatenate((xpart, mpart[..., None]), axis=-1)
 
     ncomb = ((p+3)*(p+2)*(p+1)) // 6
 
-    if iminmax is None:
-        iminmax = jnp.array([0, len(interactions)], dtype=jnp.int32)
+    if irange is None:
+        irange = jnp.array([0, len(interactions)], dtype=jnp.int32)
     if interactions_per_block is None:
         interactions_per_block = np.clip(len(interactions) // (4096), 2, 32)
 
     out_type = jax.ShapeDtypeStruct(xnodes.shape[:-1] + (ncomb,), xnodes.dtype)
     loc = jax.ffi.ffi_call("ilist_leaf2node_m2l", (out_type,))(
-        xnodes, xm, isplit, jnp.abs(interactions), iminmax, p=np.int32(p), 
+        xnodes, xm, isplit, jnp.abs(interactions), irange, p=np.int32(p), 
         interactions_per_block=np.uint64(interactions_per_block), 
         epsilon=np.float32(eps))[0]
     
     return loc
-ilist_leaf_to_local.jit = jax.jit(ilist_leaf_to_local, static_argnames=("p", "eps", "interactions_per_block"))
+ilist_leaf_to_node.jit = jax.jit(ilist_leaf_to_node, static_argnames=("p", "eps", "interactions_per_block"))
