e6ecab6afd8dbfe934cabe40cb1fc64fd2954b42
Jens Stücker <jens.stuecker@univie.ac.at>
2025-08-17 17:30:26 +0200

    Updated plugin to cfg based variants

--- src/custom_jax/fmdj_plugin.py
+++ src/custom_jax/fmdj_plugin.py
@@ -1,18 +1,20 @@
 # custom_jax/fmdj_plugin.py
-from fmdj.variants import variant, has_gpu, has_pkg, register_variant
+from fmdj.variants import Variant, VariantDict, vm, has_gpu
 import custom_jax as cj
 import jax.numpy as jnp
 
+def test_func(x, cfg):
+    return x
 
-def test_func(x, p=3.):
-    return x - p
-
-name = "cj"
+tag = "cj"
 priority = 1
-def only_register_if():
-    return has_gpu() and has_pkg("custom_jax")
-def reg(op, fn):
-    register_variant(op, name=name, fn=fn, priority=priority, only_if=only_register_if)
+def only_register_if(cfg):
+    return has_gpu()
+# def reg(op, fn):
+#     vm.reg
+    # register_variant(op, name=name, fn=fn, priority=priority, only_if=only_register_if)
+
+variants = VariantDict()
 
 def _ilist_leaf_to_leaf_cj(xpart, mpart, leaf_bounds, interactions, irange, max_leaf_size=64, eps=0.):
     print("cj ilist_leaf_to_leaf called with eps=%f" % eps)
@@ -20,7 +22,11 @@ def _ilist_leaf_to_leaf_cj(xpart, mpart, leaf_bounds, interactions, irange, max_
     return cj.forces.ilist_fphi(xm, leaf_bounds, jnp.abs(interactions), irange, eps=eps)[...,3]*2
 
 def register():
-    reg("testfunc", fn=test_func)
-    reg("ilist_leaf_to_leaf", fn=_ilist_leaf_to_leaf_cj)
+    if has_gpu():
+        variants.testfunc[tag] = Variant(test_func)
+        variants.ilist_leaf_to_leaf[tag] = Variant(_ilist_leaf_to_leaf_cj)
 
-    print("Custom Jax Plugin registered!")
+        vm.register_variants(variants)
+        print("Custom Jax Plugin registered!")
+    else:
+        print("Could not find GPU, Custom Jax Plugin not registered.")
