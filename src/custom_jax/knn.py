Fixed extend calculation. Implemented interaction counting kernel

src/custom_jax/knn.py
@@ -51,12 +51,14 @@ def build_ilist_knn(xleaf, lvl_leaf, npart_leaf, isplit, node_ilist, node_ilist_
     leaf_ilist = jax.ShapeDtypeStruct((128 * len(xleaf),), jnp.int32)
     leaf_ilist_splits = jax.ShapeDtypeStruct((len(xleaf)+1,), jnp.int32)
 
-    res = jax.ffi.ffi_call("ConstructIlist", (tmp_buf, leaf_ilist, leaf_ilist_splits))(
+    cr, il, ispl = jax.ffi.ffi_call("ConstructIlist", (tmp_buf, leaf_ilist, leaf_ilist_splits))(
         x4leaf, npart_leaf, isplit, node_ilist, node_ilist_splits,
         k=np.int32(k), boxsize=np.float32(boxsize)
     )
+    
+    counts, radii = cr[0], cr[1].view(jnp.float32)
 
-    return res
+    return counts, radii, il, ispl
 
 
 def box_dist2(c1, c2, s1, s2, mode="shortest"):