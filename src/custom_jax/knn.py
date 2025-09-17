7920829e90f235409b849f409e31a0a4809b5702
Jens Stücker <jens.stuecker@univie.ac.at>
2025-09-18 00:51:45 +0200

    removed deprecated code

--- src/custom_jax/knn.py
+++ src/custom_jax/knn.py
@@ -38,7 +38,6 @@ def ilist_knn_search(xT, isplitT, xleaf, lvl_leaf, ilist, ir2list, ilist_splitsB
     return rknn, iknn
 ilist_knn_search.jit = jax.jit(ilist_knn_search, static_argnames=("k", "boxsize"))
 
-
 def build_ilist_knn(xleaf, lvl_leaf, npart_leaf, isplit, node_ilist, node_ir2list, node_ilist_splits, k=32, boxsize=0., 
                     alloc_fac=128):
     assert node_ilist_splits.shape[0] == isplit.shape[0], "Should both correspond to no. of nodes+1"
@@ -66,100 +65,10 @@ def build_ilist_knn(xleaf, lvl_leaf, npart_leaf, isplit, node_ilist, node_ir2lis
     return il, ir2l, ispl
 build_ilist_knn.jit = jax.jit(build_ilist_knn, static_argnames=["k", "boxsize", "alloc_fac"])
 
-
-def box_dist2(c1, c2, s1, s2, mode="shortest"):
-    """Shortest or longest distance between any points in two nodes
-    mode: "shortest" or "longest"
-    """
-    dx = jnp.abs(c1 - c2)
-    if mode == "shortest":
-        dx = jnp.maximum(dx - (s1 + s2) / 2., 0.)
-    elif mode == "longest":
-        dx = jnp.maximum(dx + (s1 + s2) / 2., 0.)
-    else:
-        raise ValueError("mode must be 'shortest' or 'longest'")
-    return jnp.sum(dx**2, axis=-1)
-
-def cumsum_starting_with_zero(x):
-    return jnp.concatenate((jnp.zeros((1,) + x.shape[1:], dtype=x.dtype), jnp.cumsum(x, axis=0)))
-
-def knn_interactions(xcent, dx, npart, bins=None, k=32, batch_size=128, alloc_fac=128):
-    """Get a (weakly) sorted interaction list between leaves of an octree.
-
-    xcent : centers of leaves
-    npart : number of particles in each leaf
-    dx : extend of the leaves
-    bins : bins for discretizing the distances to determine the necessary interaciton radius
-    k : number of neighbors to include in the interaction list"""
-    
-    if bins is None:
-        bins = jnp.logspace(-0.5, 1., 32)
-    nleaves = len(xcent)
-
-    def handle_single_leaf(ileaf):
-        # We want to find the smallest distance at which we are guaranteed to find >= k neighbors
-        # For this we need to include leaves whenever they exceed the distance where they are fully
-        # included by every particle in the source leaf
-
-        rbase2 = jnp.sum(dx[ileaf]**2, axis=-1)
-
-        dist2 = box_dist2(xcent[ileaf], xcent, dx, dx, mode="longest")
-
-        dratio2 = dist2 / rbase2
-
-        # bins, nkincl tells us how many neighbours are at least included at which distance:
-        nkincl = jnp.cumsum(jnp.histogram(dratio2, bins=bins, weights=npart)[0])
-        r2min = jnp.min(jnp.where(nkincl >= k, bins[1:], jnp.inf), axis=-1) * rbase2
-
-        # To count the leaves we need to check at a given radius, we need to compare the closest
-        # distance
-        dist2min = box_dist2(xcent[ileaf], xcent, dx[ileaf], dx, mode="shortest")
-        ninteractions = jnp.sum(dist2min <= r2min, axis=-1)
-
-        return jnp.sqrt(r2min), ninteractions
-    
-    rneed, ninteractions = jax.lax.map(handle_single_leaf, jnp.arange(nleaves), batch_size=batch_size)
-
-    offsets = cumsum_starting_with_zero(ninteractions)
-
-    interactions = jnp.zeros(alloc_fac * nleaves, dtype=jnp.int32)
-    
-    def insert_interactions(ileaf, interactions):
-        dist2 = box_dist2(xcent[ileaf], xcent, dx[ileaf], dx, mode="shortest")
-        dist2b = box_dist2(xcent[ileaf], xcent, dx[ileaf], dx, mode="longest")
-
-        isort = jnp.lexsort([dist2b, dist2])
-
-        iarange = jnp.arange(nleaves)
-        ninleaf = offsets[ileaf + 1] - offsets[ileaf]
-        ioff = jnp.where(iarange < ninleaf, offsets[ileaf] + iarange, offsets[-1])
-
-        return interactions.at[ioff].set(isort)
-
-    interactions = jax.lax.fori_loop(0, nleaves, insert_interactions, interactions)
-
-    return rneed, interactions, offsets
-knn_interactions.jit = jax.jit(knn_interactions, static_argnames=["k", "batch_size", "alloc_fac"])
-
 def dense_ilist(num):
     ilist = jnp.array((jnp.arange(num, dtype=jnp.int32),)*num)
     isplits = jnp.arange(num+1, dtype=jnp.int32)*num
     return ilist.flatten(), isplits
-
-def brute_force_node_ilist_prep(octree, k=16):
-    """Helps preparing the inputs for build_ilist_knn"""
-    npart_leaf = octree.leaf_particle_bounds[1:] - octree.leaf_particle_bounds[:-1]
-    level_leaf = octree.level_binary[octree.node_of_leaf] - 1
-    leaf_cent, leaf_ext = get_node_box(octree.xleaf, level_leaf)
-
-    nleaves = int(octree.nnodes - 1 )
-    nnodes = nleaves // 32 + 1
-    isplit = jnp.clip(jnp.arange(0, nnodes+1, dtype=jnp.int32)*32, 0, nleaves)
-    # node_ilist = jnp.array((jnp.arange(nnodes, dtype=jnp.int32),)*nnodes)
-    # node_ilist_splits = jnp.arange(nnodes+1, dtype=jnp.int32)*nnodes
-    node_ilist, node_ilist_splits = dense_ilist(nnodes)
-
-    return leaf_cent, level_leaf, npart_leaf, isplit, node_ilist, node_ilist_splits
 
 def build_ilist_recursive(xleaf, lvleaf, nleaf, max_size=48, num_part=None, 
         refine_fac=8, k=16, stop_coarsen=128, boxsize=0., alloc_fac=128.):
