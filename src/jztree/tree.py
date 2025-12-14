8668dbcdf8b87145c521053e0e1575ea44b89ec3
Jens Stücker <jens.stuecker@univie.ac.at>
2025-12-14 21:45:49 +0100

    use tree from fmdj

--- src/jztree/tree.py
+++ src/jztree/tree.py
@@ -4,39 +4,4 @@ import jax.numpy as jnp
 
 from jztree_cuda import ffi_tree
 
-jax.ffi.register_ffi_target("PosZorderSort", ffi_tree.PosZorderSort(), platform="CUDA")
-jax.ffi.register_ffi_target("SearchSortedZ", ffi_tree.SearchSortedZ(), platform="CUDA")
-
-def pos_zorder_sort(x, block_size=64):
-    assert x.dtype == jnp.float32
-    assert x.shape[-1] == 3
-
-    # To optimize memory layout, we bundle position and id together into a single array
-    # We later need to reinterprete the output to extract positions and ids
-    out_type = jax.ShapeDtypeStruct((x.shape[0],4), jnp.int32)
-    # This is a guess, how much a temporary storage in cub::DeviceMergeSort::SortKeys requires
-    # If we estimate too little, an error will be thrown from the C++ code:
-    tmp_buff_type = jax.ShapeDtypeStruct((x.shape[0] + np.maximum(1024, x.shape[0]//16), 4), jnp.int32)
-    isort = jax.ffi.ffi_call("PosZorderSort", (out_type, tmp_buff_type), vmap_method="sequential")(x, block_size=np.uint64(block_size))[0]
-
-    pos = isort[:, :3].view(jnp.float32)
-    ids = isort[:, 3].view(jnp.int32)
-
-    return pos, ids
-pos_zorder_sort.jit = jax.jit(pos_zorder_sort, static_argnames=("block_size",))
-
-def search_sorted_z(xz, xz_query, block_size=64, leaf_search=False):
-    """Finds the indices in xz where elements of xz_query would be inserted to keep order.
-    This is similar to np.searchsorted, but works for 3D points sorted in Z-order.
-    On equality maintains the rule: xz[idx] < v <= xz[idx+1]
-    if leaf_search is True, it is assumed that xz contains one point per leaf and we 
-    return the index of the leaf that the query point belongs to.
-    """
-    assert xz.dtype ==  xz_query.dtype == jnp.float32
-    assert xz.shape[-1] == xz_query.shape[-1] == 3
-
-    out_type = jax.ShapeDtypeStruct((xz_query.shape[0],), jnp.int32)
-    inds = jax.ffi.ffi_call("SearchSortedZ", (out_type,))(
-        xz, xz_query, block_size=np.uint64(block_size), leaf_search=leaf_search)[0]
-    return inds
-search_sorted_z.jit = jax.jit(search_sorted_z, static_argnames=("block_size", "leaf_search"))
+from fmdj.ztree import pos_zorder_sort, search_sorted_z
