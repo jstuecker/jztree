cf1014e11cf0d1e260f3c7df199c3eecc56e0e3f
Jens Stücker <jens.stuecker@univie.ac.at>
2026-01-20 22:25:10 +0100

    removed outdated node-node-fof test

--- checks/tests/test_multi_gpu.py
+++ checks/tests/test_multi_gpu.py
@@ -62,42 +62,6 @@ def particles_and_tree(seed=0):
     return fmdj.ztree.distr_zsort_and_tree(part, npart_tot, cfg.tree)
 
 @jax.jit
-@jax.shard_map(in_specs=P(), out_specs=P("gpus"), mesh=mesh)
-def node_fof(seed=0):
-    rank, ndev, axis_name = get_rank_info()
-    partz, th = particles_and_tree(seed)
-
-    node_data, ilist = jztree.fof.distr_node_node_fof.jit(th, rlink=0.8)
-    labels, spl = node_data.label, node_data.spl
-    nnodes = jnp.argmax(spl)
-
-    num = jax.lax.all_gather(nnodes, axis_name)
-    dspl = cumsum_starting_with_zero(num)
-
-    igroup = dspl[labels.irank] + labels.igroup
-
-    return partz, igroup.reshape(1,-1), ilist.ispl[-1].reshape(1), dspl.reshape(1,-1)
-
-@pytest.mark.skipif(jax.device_count() <= 1, reason="Requires multiple devices")
-@pytest.mark.parametrize("seed", [0,11,77,133,1337])
-def test_distr_node_node_fof(seed):
-    partz, igroup1, numint1, dev_spl = node_fof(seed)
-    # combine arrays into one:
-    igroup1 = multi_to_dense.jit(igroup1, dev_spl[0])
-    numint1 = jnp.sum(numint1)
-
-    cfg = jztree.data.FofConfig()
-    partz = fmdj.ztree.pos_zorder_sort(partz)[0]
-    th = fmdj.ztree.build_tree_hierarchy.jit(partz, cfg.tree, npart_tot=int(jnp.sum(~jnp.isnan(partz.pos[...,0]))))
-    node_data, ilist2 = jztree.fof.node_node_fof.jit(th, rlink=0.8)
-
-    assert igroup1[:len(node_data.label)] == pytest.approx(node_data.label, abs=0.1)
-    # So far, there will be more interactions in the multi-gpu case, because the pruning based
-    # on identical ids is less efficient, since the global true ids are not perfectly known at that
-    # point. For now, just check that we have at least as many interactions:
-    assert numint1 >= ilist2.ispl[-1]
-
-@jax.jit
 @jax.shard_map(out_specs=P("gpus"), in_specs=P(), mesh=mesh)
 def distr_fof(seed):
     rank, ndev, axis_name = get_rank_info()
