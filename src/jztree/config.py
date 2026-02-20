313649df65462f1aa3da2f53275f67580466585a
Jens Stücker <jens.stuecker@univie.ac.at>
2026-02-20 17:40:33 +0100

    removed duplicate TreeConfig

--- src/jztree/config.py
+++ src/jztree/config.py
@@ -22,22 +22,6 @@ class TreeConfig():
     mass_centered: bool = True
 
 @dataclass(unsafe_hash=True)
-class TreeConfig():
-    # structure:
-    max_leaf_size: int = 32
-    coarse_fac: float = 6.0
-    stop_coarsen: int = 1024
-
-    # memory usage:
-    alloc_fac_nodes: float = 1.0
-
-    # distributed sort:
-    nsamp: int = 1024
-
-    # other:
-    mass_centered: bool = True
-
-@dataclass(unsafe_hash=True)
 class FofCatalogueConfig():
     npart_min: int = 20
 
