7caa1ab4df9bbc8326bbbbf5b60816a23cc949d8
Jens Stücker <jens.stuecker@univie.ac.at>
2026-02-22 12:29:06 +0100

    added a steps benchmark to knn

--- checks/benchmarks/bench_distr_knn.py
+++ checks/benchmarks/bench_distr_knn.py
@@ -27,5 +27,33 @@ def bench_distr_knn(jax_bench, N):
     part = ics.uniform_particles.smap(mesh, jit=True)(N, npad=int(N * 0.4))
 
     rnn = jb.measure(fn_jit=knn.distr_knn.smap(mesh, jit=True),
-        part=part, k=12, output_order="input", result="rad", tag=f"ndev{ndev}"
+        part=part, k=16, output_order="input", result="rad", tag=f"ndev{ndev}"
+    )
+
+@pytest.mark.shrink_in_quick(keep_index=1)
+@pytest.mark.skipif(jax.device_count() <= 1, reason="Requires multiple devices")
+def bench_distr_knn_steps(jax_bench):
+    N = int(1e7)
+
+    cfg = KNNConfig()
+    cfg.tree.alloc_fac_nodes = 2.0
+
+    ndev = jax.device_count()
+
+    jb = jax_bench(jit_rounds=4, jit_warmup=1)
+
+    mesh = get_mesh(4)
+
+    part = ics.uniform_particles.smap(mesh, jit=True)(N, npad=int(N * 0.4))
+
+    partz, th = jb.measure(fn_jit=knn.distr_zsort_and_tree.smap(mesh, jit=True),
+        part=part, cfg_tree=cfg.tree, tag="tree_ndev{ndev}"
+    )[1]
+
+    ilist = jb.measure(fn_jit=knn._knn_dual_walk.smap(mesh, jit=True),
+        th=th, k=16, alloc_fac_ilist=cfg.alloc_fac_ilist, tag="treewalk_ndev{ndev}"
+    )
+
+    rnn = jb.measure(fn_jit=knn.distr_knn.smap(mesh, jit=True),
+        part=partz, k=16, output_order="z", result="rad", tag=f"totalz_ndev{ndev}"
     )
