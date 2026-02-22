70453e77beff4ab6b87c7d9d0ad90e623a36bff1
Jens Stücker <jens.stuecker@univie.ac.at>
2026-02-22 16:37:12 +0100

    added yet-failing gaussian  benchmark

--- checks/benchmarks/bench_distr_knn.py
+++ checks/benchmarks/bench_distr_knn.py
@@ -30,6 +30,24 @@ def bench_distr_knn(jax_bench, N):
         part=part, k=16, output_order="z", result="rad", tag=f"ndev{ndev}"
     )
 
+# @pytest.mark.parametrize("N", (int(1e6), int(1e7), int(1e8)))
+# @pytest.mark.skipif(jax.device_count() <= 1, reason="Requires multiple devices")
+# def bench_gaus(jax_bench, N):
+#     cfg = KNNConfig()
+#     cfg.tree.alloc_fac_nodes = 4.0
+
+#     ndev = jax.device_count()
+
+#     jb = jax_bench(jit_rounds=4, jit_warmup=1)
+
+#     mesh = get_mesh(4)
+
+#     part = ics.gaussian_particles.smap(mesh, jit=True)(N, npad=int(N * 0.8))
+
+#     rnn = jb.measure(fn_jit=knn.distr_knn.smap(mesh, jit=True),
+#         part=part, k=16, output_order="z", result="rad", tag=f"ndev{ndev}"
+#     )
+
 @pytest.mark.shrink_in_quick(keep_index=1)
 @pytest.mark.skipif(jax.device_count() <= 1, reason="Requires multiple devices")
 def bench_distr_knn_steps(jax_bench):
