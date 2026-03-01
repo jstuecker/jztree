7a8c7a965bde6c979dec2b247e81e0cb1508e70e
Jens Stücker <jens.stuecker@univie.ac.at>
2026-03-01 11:28:10 +0100

    improved zsort benchmark

--- checks/benchmarks/bench_tree.py
+++ checks/benchmarks/bench_tree.py
@@ -25,16 +25,14 @@ def bench_build_tree_hierarchy(jax_bench, npart):
         fn=build_tree_hierarchy, fn_jit=build_tree_hierarchy.jit, 
         part=pos_mass_z, cfg_tree=cfg_tree, tag="mass_centered")
 
-@pytest.mark.shrink_in_quick(keep_index=3)
+@pytest.mark.shrink_in_quick(keep_index=2)
 @pytest.mark.parametrize("npart", [1024*128,1024*1024, 1024*1024*8, 1024*1024*32])
 def bench_zsort(jax_bench, npart):
-    pos_mass = ics.uniform_particles(npart)
-    jb = jax_bench(jit_rounds=50, jit_warmup=5)
+    jb = jax_bench(jit_rounds=5, jit_loops=5, jit_warmup=1)
 
-    jb.measure(fn_jit=pos_zorder_sort.jit, x=pos_mass.pos, tag="zsort")
+    def f():
+        pos = ics.uniform_particles(npart)
+        return pos_zorder_sort(pos)
+    f.jit = jax.jit(f)
 
-    irand = jax.random.randint(jax.random.key(0), npart, 0, 128)
-    @jax.jit
-    def argsort(i):
-        return jnp.argsort(i, stable=False)
-    jb.measure(fn_jit=argsort, i=irand, tag="argsort")
+    jb.measure(fn_jit=f.jit, tag="zsort")
